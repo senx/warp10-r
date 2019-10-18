5cfad0f52073415ec9f663aaac41a64020cd2d66
Denis Roussel <droussel@centreon.com>
2019-10-18 16:15:50 +0200

    :zap: Again, improve performance to fetch data

--- R/execute.R
+++ R/execute.R
@@ -17,7 +17,7 @@ wrp_exec <- function(wrp_con) {
     return(cat(wrp_script))
   }
 
-  res <- jsonlite::fromJSON(raw_res, simplifyDataFrame = FALSE)
+  res <- jsonlite::fromJSON(raw_res, simplifyVector = FALSE)
 
   if (length(stack) == 1) {
     build_res(stack[[1]], res[[1]])
@@ -43,7 +43,7 @@ build_res.default <- function(object, data) {
 #'
 build_res.gts <- function(object, data) {
   new_data                <- as.data.frame(data[["v"]])
-  if (length(new_data) >= 2) {
+  if (length(new_data) >= 2 && c("V1", "V2") %in% names(new_data)) {
     names(new_data)         <- c("timestamp", "value", names(new_data)[-c(1, 2)])
     new_data[["timestamp"]] <- lubridate::as_datetime(new_data[["timestamp"]] / 1e6)
   }
@@ -63,28 +63,39 @@ build_res.gts <- function(object, data) {
 #'
 build_res.lgts <- function(object, data) {
   metadata        <- list()
-  new_data        <- purrr::map(data, function(x) as.data.frame(x[["v"]]))
+  n_values        <- purrr::map_int(data, ~ length(.x[["v"]]))
   classes         <- purrr::map_chr(data, "c")
-  labels          <- purrr::map(data, "l")
-  labels_df       <- dplyr::bind_rows(labels)
-  other_names     <- NULL
+  labels_df       <- purrr::map_dfr(data, "l")
+  is_value        <- all(n_values > 0L)
+  new_data        <- if (is_value) {
+    purrr::map(data, function(l) {
+      tibble::tibble(
+        V1 = purrr::map_dbl(l[["v"]], 1),
+        V2 = purrr::map_dbl(l[["v"]], 2)
+      )
+    })
+  } else {
+    labels_df
+  }
   if (length(unique(classes)) > 1) {
-    new_data        <- purrr::map2(new_data, classes, add_col, col_name = "class")
+    if (is_value) new_data <- purrr::map2(new_data, classes, add_col, col_name = "class")
   } else {
     metadata[["c"]] <- classes[[1]]
+    if (!is_value) new_data[[classes[[1]]]] <- NULL
   }
   for (label in names(labels_df)) {
     if (length(unique(labels_df[[label]])) > 1) {
-      new_data        <- purrr::map2(new_data, labels_df[[label]], add_col, col_name = label)
+      if (is_value) new_data <- purrr::map2(new_data, labels_df[[label]], add_col, col_name = label)
     } else {
       metadata[["l"]] <- c(metadata[["l"]], setNames(list(labels_df[[label]][1]), label))
+      if (!is_value) new_data[[label]] <- NULL
     }
   }
 
   list_gts <- list(
     c = metadata[["c"]],
     l = metadata[["l"]],
-    v = dplyr::bind_rows(new_data)
+    v = if (is_value) dplyr::bind_rows(new_data) else new_data
   )
 
   build_res.gts(list_gts, object = "gts")
